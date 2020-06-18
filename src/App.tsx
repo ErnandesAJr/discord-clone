9b058d2d3b2f4dc8d67b32635b5d9fac9060ff4d
Ernandes Azevedo Junior <ernandes_azevedo@stash.atlantico.com.br>
2020-06-17 23:49:22 -0300

    Creating the Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import Layout from "./components/Layout";
 import GlobalStyles from "./styles/GlobalStyles";
 function App() {
   return (
     <>
+      <Layout />
+
       <h1>Vai nessa</h1>
       <GlobalStyles />
     </>
---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Grid } from "./styles";
+
+const Layout: React.FC = () => {
+  return <div />;
+};
+
+export default Layout;
